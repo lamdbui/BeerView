37124c4a2e5eed48b80bd8a7e6219621c9f00670
Lam Bui <lamdbui@gmail.com>
2017-04-26 17:04:06 -0700

    Use live data from BreweryDB and leverage ButterKnife

--- app/src/main/java/app/com/lamdbui/android/beerview/BeerViewActivityFragment.java
+++ app/src/main/java/app/com/lamdbui/android/beerview/BeerViewActivityFragment.java
@@ -3,6 +3,8 @@ package app.com.lamdbui.android.beerview;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,8 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,8 +28,14 @@ public class BeerViewActivityFragment extends Fragment {
     private static final String LOG_TAG = BeerViewActivityFragment.class.getSimpleName();
     private static final String API_KEY = BuildConfig.BREWERY_DB_API_KEY;
 
-    private TextView mBodyTextView;
-    private String mBreweryStr;
+    @BindView(R.id.body_text)
+    TextView mBodyTextView;
+    @BindView(R.id.brewery_recycler_view)
+    RecyclerView mBreweryRecyclerView;
+
+    private BreweryAdapter mBreweryAdapter;
+
+    private List<Brewery> mBreweries;
 
     public BeerViewActivityFragment() {
     }
@@ -34,21 +44,7 @@ public class BeerViewActivityFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // test data
-        //List<Brewery> breweries = new ArrayList<>();
-        Brewery brewery = new Brewery();
-        brewery.setId("ABC");
-        brewery.setName("TapLam");
-        brewery.setStreetAddress("1234 Hoppy Lane");
-        brewery.setLocality("Beerland");
-        brewery.setRegion("BL");
-        brewery.setPostalCode("99991");
-        brewery.setPhone("555-111-222");
-        brewery.setWebsite("http://www.beer.com");
-        brewery.setLatitude(37.774929);
-        brewery.setLongitude(-122.419416);
-
-        mBreweryStr = brewery.toString();
+        mBreweries = new ArrayList<>();
 
         BreweryDbInterface breweryDbInterface =
                 BreweryDbClient.getClient().create(BreweryDbInterface.class);
@@ -57,8 +53,11 @@ public class BeerViewActivityFragment extends Fragment {
         call.enqueue(new Callback<BreweryResponse>() {
             @Override
             public void onResponse(Call<BreweryResponse> call, Response<BreweryResponse> response) {
-                List<Brewery> breweries = response.body().getData();
-                Log.d(LOG_TAG, "CAN HAZ");
+                // TODO: Maybe check for bad data?
+                mBreweries = response.body().getData();
+                if(mBreweryAdapter == null)
+                    mBreweryAdapter = new BreweryAdapter(mBreweries);
+                mBreweryRecyclerView.setAdapter(mBreweryAdapter);
             }
 
             @Override
@@ -72,10 +71,60 @@ public class BeerViewActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_beer_view, container, false);
+        ButterKnife.bind(this, rootView);
 
-        mBodyTextView = (TextView) rootView.findViewById(R.id.body_text);
-        mBodyTextView.setText(mBreweryStr);
+        mBreweryRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return rootView;
+    }
+
+    private class BreweryHolder extends RecyclerView.ViewHolder {
+
+        private TextView mBodyTextView;
+
+        private Brewery mBrewery;
+
+        public BreweryHolder(View itemView) {
+            super(itemView);
+
+            mBodyTextView = (TextView) itemView.findViewById(R.id.list_item_brewery_body_text);
+        }
+
+        public void bind(Brewery brewery) {
+            mBrewery = brewery;
+            mBodyTextView.setText(mBrewery.toString());
+        }
+    }
+
+    private class BreweryAdapter extends RecyclerView.Adapter<BreweryHolder> {
+
+        private List<Brewery> mBreweries;
+
+        public BreweryAdapter(List<Brewery> breweries) {
+            mBreweries = breweries;
+        }
+
+        @Override
+        public BreweryHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.list_item_brewery, parent, false);
+
+            return new BreweryHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(BreweryHolder holder, int position) {
+            Brewery brewery = mBreweries.get(position);
+            holder.bind(brewery);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mBreweries.size();
+        }
+
+        public void setBreweries(List<Brewery> breweries) {
+            mBreweries = breweries;
+        }
     }
 }
