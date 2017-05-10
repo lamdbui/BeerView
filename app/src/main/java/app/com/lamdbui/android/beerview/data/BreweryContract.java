86eedd9dba5048ce1a8342fd306ea684f40de37e
Lam Bui <lamdbui@gmail.com>
2017-05-09 22:56:58 -0700

    Being setting up structure for Brewery database and Contract

--- app/src/main/java/app/com/lamdbui/android/beerview/data/BreweryContract.java
+++ app/src/main/java/app/com/lamdbui/android/beerview/data/BreweryContract.java
@@ -1,6 +1,8 @@
 package app.com.lamdbui.android.beerview.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 /**
  * Created by lamdbui on 5/2/17.
@@ -13,5 +15,51 @@ public class BreweryContract {
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     // possible paths
-    public static final String PATH_BREWERIES = "breweries";
+    public static final String PATH_BREWERY = "brewery";
+
+    // class that defines the Brewery Table Schema
+    public static final class BreweryTable implements BaseColumns {
+
+        public static final String TABLE_NAME = "brewery";
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_BREWERY).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BREWERY;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BREWERY;
+
+        public static final class COLS {
+
+            // from the Brewery class
+            public static final String ID = "id";
+            public static final String NAME = "name";
+            public static final String NAME_SHORT = "name_short";
+            public static final String DESCRIPTION = "description";
+            public static final String WEBSITE = "website";
+            public static final String HOURS_OF_OPERATION = "hours_of_operation";
+            public static final String ESTABLISHED = "established";
+            public static final String IS_ORGANIC = "is_organic";
+            public static final String IMAGES_ICON = "images_icon";
+            public static final String IMAGES_MEDIUM = "images_medium";
+            public static final String IMAGES_LARGE = "images_large";
+            public static final String IMAGES_SQUARE_MEDIUM = "images_square_medium";
+            public static final String IMAGES_SQUARE_LARGE = "images_square_large";
+            public static final String ADDRESS = "address";
+            public static final String LOCALITY = "locality";
+            public static final String REGION = "region";
+            public static final String POSTAL_CODE = "postal_code";
+            public static final String PHONE = "phone";
+            public static final String LATITUDE = "latitude";
+            public static final String LONGITUDE = "longitude";
+            public static final String IS_PRIMARY = "is_primary";
+            public static final String IS_PLANNING = "is_planning";
+            public static final String IS_CLOSED = "is_closed";
+            public static final String OPEN_TO_PUBLIC = "open_to_public";
+            public static final String LOCATION_TYPE = "location_type";
+            public static final String LOCATION_TYPE_DISPLAY = "location_tyoe_display";
+            public static final String COUNTRY_ISO_CODE = "country_iso_code";
+        }
+    }
 }
--- app/src/main/java/app/com/lamdbui/android/beerview/data/BreweryDbHelper.java
+++ app/src/main/java/app/com/lamdbui/android/beerview/data/BreweryDbHelper.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import app.com.lamdbui.android.beerview.Brewery;
+import app.com.lamdbui.android.beerview.data.BreweryContract.BreweryTable;
+
 /**
  * Created by lamdbui on 5/2/17.
  */
@@ -11,37 +14,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class BreweryDbHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "breweries.db";
-
-//    private String mId;
-//    private String mName;
-//    private String mNameShort;
-//    private String mDescription;
-//    private String mWebsite;
-//    private String mHoursOfOperation;
-//    private int mEstablished;
-//    private boolean mIsOrganic;
-//    private String mImagesIcon;
-//    private String mImagesMedium;
-//    private String mImagesLarge;
-//    private String mImagesSquareMedium;
-//    private String mImagesSquareLarge;
-//    private String mStreetAddress;
-//    private String mLocality;
-//    private String mRegion;
-//    private String mPostalCode;
-//    private String mPhone;
-//    private double mLatitude;
-//    private double mLongitude;
-//    private boolean mIsPrimary;
-//    private boolean mIsPlanning;
-//    private boolean mIsClosed;
-//    private boolean mOpenToPublic;
-//    private String mLocationType;
-//    private String mLocationTypeDisplay;
-//    private String mCountryIsoCode;
-
-    //final String SQL_CREATE_BREWERY_TABLE = "CREATE TABLE "
+    public static final String DATABASE_NAME = "brewery.db";
 
     public BreweryDbHelper(Context context, SQLiteDatabase.CursorFactory factory) {
         super(context, DATABASE_NAME, factory, DATABASE_VERSION);
@@ -50,10 +23,75 @@ public class BreweryDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
+//        public static final String ID = "id";
+//        public static final String NAME = "name";
+//        public static final String NAME_SHORT = "name_short";
+//        public static final String DESCRIPTION = "description";
+//        public static final String WEBSITE = "website";
+//        public static final String HOURS_OF_OPERATION = "hours_of_operation";
+//        public static final String ESTABLISHED = "established";
+//        public static final String IS_ORGANIC = "is_organic";
+//        public static final String IMAGES_ICON = "images_icon";
+//        public static final String IMAGES_MEDIUM = "images_medium";
+//        public static final String IMAGES_LARGE = "images_large";
+//        public static final String IMAGES_SQUARE_MEDIUM = "images_square_medium";
+//        public static final String IMAGES_SQUARE_LARGE = "images_square_large";
+//        public static final String ADDRESS = "address";
+//        public static final String LOCALITY = "locality";
+//        public static final String REGION = "region";
+//        public static final String POSTAL_CODE = "postal_code";
+//        public static final String PHONE = "phone";
+//        public static final String LATITUDE = "latitude";
+//        public static final String LONGITUDE = "longitude";
+//        public static final String IS_PRIMARY = "is_primary";
+//        public static final String IS_PLANNING = "is_planning";
+//        public static final String IS_CLOSED = "is_closed";
+//        public static final String OPEN_TO_PUBLIC = "open_to_public";
+//        public static final String LOCATION_TYPE = "location_type";
+//        public static final String LOCATION_TYPE_DISPLAY = "location_tyoe_display";
+//        public static final String COUNTRY_ISO_CODE = "country_iso_code";
+
+        // Create the table for the Breweries
+        final String SQL_CREATE_BREWERY_TABLE = "CREATE TABLE "
+                + BreweryTable.TABLE_NAME + " ("
+                + BreweryTable._ID + " INTEGER PRIMARY KEY, "
+                + BreweryTable.COLS.ID + " TEXT UNIQUE NOT NULL, "
+                + BreweryTable.COLS.NAME + " TEXT NOT NULL, "
+                + BreweryTable.COLS.NAME_SHORT + " TEXT, "
+                + BreweryTable.COLS.DESCRIPTION + " TEXT, "
+                + BreweryTable.COLS.WEBSITE + " TEXT, "
+                + BreweryTable.COLS.HOURS_OF_OPERATION + " TEXT, "
+                + BreweryTable.COLS.ESTABLISHED + " INTEGER, "
+                + BreweryTable.COLS.IS_ORGANIC + " INTEGER, "
+                + BreweryTable.COLS.IMAGES_ICON + " TEXT, "
+                + BreweryTable.COLS.IMAGES_MEDIUM + " TEXT, "
+                + BreweryTable.COLS.IMAGES_LARGE + " TEXT, "
+                + BreweryTable.COLS.IMAGES_SQUARE_MEDIUM + " TEXT, "
+                + BreweryTable.COLS.IMAGES_SQUARE_LARGE + " TEXT, "
+                + BreweryTable.COLS.ADDRESS + " TEXT, "
+                + BreweryTable.COLS.LOCALITY + " TEXT, "
+                + BreweryTable.COLS.REGION + " TEXT, "
+                + BreweryTable.COLS.POSTAL_CODE + " TEXT, "
+                + BreweryTable.COLS.PHONE + " TEXT, "
+                + BreweryTable.COLS.LATITUDE + " REAL , "
+                + BreweryTable.COLS.LONGITUDE + " REAL, "
+                + BreweryTable.COLS.IS_PRIMARY + " INTEGER, "
+                + BreweryTable.COLS.IS_PLANNING + " INTEGER, "
+                + BreweryTable.COLS.IS_CLOSED + " INTEGER, "
+                + BreweryTable.COLS.OPEN_TO_PUBLIC + " INTEGER, "
+                + BreweryTable.COLS.LOCATION_TYPE + " TEXT, "
+                + BreweryTable.COLS.LOCATION_TYPE_DISPLAY + " TEXT, "
+                + BreweryTable.COLS.COUNTRY_ISO_CODE + " TEXT "
+                + ");";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_BREWERY_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        // drop and recreate the tables for now.
+        // we probably want to do something better in the future...
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + BreweryTable.TABLE_NAME);
+        onCreate(sqLiteDatabase);
     }
 }
